59b82e23979cc318c89ab10da170f5bbdf59edb2
theophilus adewale <adewaletheophilus@gmail.com>
2024-11-04 02:38:12 +0100

    Nativewind Working

--- AgeBuddy/app/index.tsx
+++ AgeBuddy/app/index.tsx
@@ -1,15 +1,12 @@
+import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="text-2xl text-red-500">Home Page</Text>
-    </View>
+    <SafeAreaView className="flex-1 justify-center items-center">
+      <StatusBar backgroundColor="#fff" />
+      <Text className="text-3xl font-bold">Welcome</Text>
+    </SafeAreaView>
   );
 }
---
+++ AgeBuddy/constants/COLORS.tsx
@@ -0,0 +1,16 @@
+// export default COLOR = {
+//   white: "#ffffff",
+//   black: "#000000",
+//   loginText: "#000090",
+//   onBoardCardBG: "#EBCBFA",
+//   bgLineGradOne: "#FFFEFE",
+//   bgLineGradTwo: "#F6F5FC",
+//   bgLineGradThree: "#EEEBF9",
+//   bgLineGradFour: "#E3#6F6",
+//   bgLineGradFive: "#0CE2EC",
+//   bgLineGradSix: "#E50EE4",
+//   transparent: "transparent",
+
+//   warning: "#F88070",
+//   accent: "#FF6060",
+// };
